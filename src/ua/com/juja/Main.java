3e2a7fe075f90e3735e17243673d144eedf75053
Andrei Maksimov <magcraft@gmail.com>
2017-05-23 18:04:58 +0200

    Реализация методов SELECT, INSERT, UPDATE, DELETE в ООП стиле :)

--- src/ua/com/juja/Main.java
+++ src/ua/com/juja/Main.java
@@ -1,9 +1,7 @@
 package ua.com.juja;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Random;
 
 /**
  * Created by magcraft on 19/05/2017.
@@ -15,10 +13,37 @@ public class Main {
         Connection connection = DriverManager.getConnection(
                     "jdbc:postgresql://10.211.55.6:5432/SQLCMD", "postgres",
                     "buh1762");
-        //add a row
+
+        //Insert add a row
         Statement stmt = connection.createStatement();
         stmt.executeUpdate("INSERT INTO public.users (name, pass)"+
                 "VALUES ('from java code', 'zxc')");
+        stmt.close();
+
+
+        //DELETE
+        stmt = connection.createStatement();
+        stmt.executeUpdate("DELETE FROM public.users WHERE id > 12");
+        stmt.close();
+
+        //update
+        PreparedStatement ps = connection.prepareStatement("UPDATE public.users SET pass = ? WHERE id > 2");
+        String pass = "@_" + new Random().nextInt();
+        ps.setString(1, pass);
+        ps.executeUpdate();
+        ps.close();
+
+        //SELECT
+        stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM public.users WHERE id > 3");
+        while (rs.next()) {
+            System.out.println("id: " + rs.getString("id"));
+            System.out.println("name: " + rs.getString("name"));
+            System.out.println("pass: " + rs.getString("pass"));
+            System.out.println("========================================");
+        }
+        rs.close();
+        stmt.close();
 
         connection.close();
     }
